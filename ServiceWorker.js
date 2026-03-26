ab18be9d7912de465d35850a5e8742b378e6e8a5
hamachan64 <yuuki.hama2186@gmail.com>
2026-03-26 20:16:11 +0900

    build: Update Unity WebGL data file hash in service worker and index HTML.

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -2,7 +2,7 @@ const cacheName = "DefaultCompany-DegitalCard-0.1.0";
 const contentToCache = [
     "Build/be42564869e0e00592d37a73e71a946f.loader.js",
     "Build/50bec446c21d14e7133db4f13f7ff6b5.framework.js",
-    "Build/e6ce2e406dd28f2fd384aa3f331971b0.data",
+    "Build/84a99cd148cd0d59baaed593460905f4.data",
     "Build/ab6a88ace5bd12d41d53463aaa994598.wasm",
     "TemplateData/style.css"
 
